0b36eab7b126bd485cd976af08046d8d560be4df
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-06-29 21:57:51 -0400

    added text to circles in projects, added dotted line

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,15 +4,21 @@ import React from 'react';
 export default function Projects() {
   // Define your projects data or fetch it from an API
 
-  const renderCircles = (count) => {
+  const renderCircles = (count, texts) => {
     const circles = [];
     for (let i = 0; i < count; i++) {
       circles.push(
-        <div className="circle" key={i}></div>
+        <div className="circle" key={i}>
+          <div className="circle-text">{texts[i]}</div>
+        </div>
       );
     }
     return circles;
   };
+
+  const interestTexts = ['Interest 1', 'Interest 2', 'Interest 3'];
+  const technologyTextsRow1 = ['Technology 1', 'Technology 2', 'Technology 3', 'Technology 4', 'Technology 5'];
+  const technologyTextsRow2 = ['Technology 6', 'Technology 7', 'Technology 8'];
 
   return (
     <div>
@@ -20,14 +26,10 @@ export default function Projects() {
         <div className='title'>
           <h3>Interests</h3>
           <div className="circles">
-            {renderCircles(3)}
+            {renderCircles(3, interestTexts)}
           </div>
         </div>
       </div>
-      <br />
-      <br />
-      <br />
-      <br />
       <br />
       <br />
       <br />
@@ -43,16 +45,17 @@ export default function Projects() {
           <h3>Technology</h3>
           <div className="circles">
             <div className="row">
-              {renderCircles(5)}
+              {renderCircles(5, technologyTextsRow1)}
             </div>
             <div className="row">
-              {renderCircles(3)}
+              {renderCircles(3, technologyTextsRow2)}
             </div>
           </div>
+            <div className="line"></div> {/* Add the line element here */}
         </div>
-</div>
-
-
+      </div>
+      
     </div>
   );
 }
+
