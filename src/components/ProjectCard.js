4a170b16497d7b87c799d3517bb5d4ad63941d65
emilyjiji <92887308+emilyjiji@users.noreply.github.com>
2023-07-16 13:50:45 -0400

    adjusted img and title/description alignment

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -14,34 +14,34 @@ export default function ProjectCard({ title, description, interests, technology,
 
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg project-card">
-      <div className="relative">
-        <img className="w-full h-48 object-cover" src={image || dummyImage} alt={title} />
-        <div className="absolute bottom-0 left-0 bg-white p-2">{title}</div>
-      </div>
-      <div className="px-6 py-4">
-        <p className="text-base">{description}</p>
-      </div>
+    <div className="relative w-full">
+      <img className="w-full h-48 object-cover rounded-t" src={image || dummyImage} alt={title} />
+      <div className="font-bold mb-1 px-6 bt-3 pt-2 text-left" style={{ color: 'rgb(25, 42, 86)', paddingLeft: '24px' }}>{title}</div>
+    </div>
+    <div className="px-6 py-4 text-left" style={{ opacity: 0.8, color: 'rgb(25, 42, 86)' }} >
+      <p className="text-sm" >{description}</p>
+    </div>
       <div className="px-6 pt-4 pb-2">
         {interests.map((interest, index) => (
-          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2">
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2" style={{color: 'rgb(25, 42, 86)'}}>
             #{interest}
           </span>
         ))}
       </div>
       <div className="px-6 pb-2">
         {technology.map((tech, index) => (
-          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 text-blue-500">
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2" style={{color: 'rgb(25, 42, 86)'}}>
             #{tech}
           </span>
         ))}
       </div>
-      <div className="px-6 pt-2 pb-4">
+      {/* <div className="px-6 pt-2 pb-4">
         {links.map((link, index) => (
-          <a key={index} href={link} target="_blank" rel="noopener noreferrer" className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2 text-blue-500">
+          <a key={index} href={link} target="_blank" rel="noopener noreferrer" className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2" style={{color: 'rgb(25, 42, 86)'}}>
             Link {index + 1}
           </a>
         ))}
-      </div>
+      </div> */}
     </div>
   );
 }
